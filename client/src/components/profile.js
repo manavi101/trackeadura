0151e1387256e30c9c85dbbd9b5be5b52991f7fc
Ignacio.Parra <ignacio.parra@minder.cc>
2020-08-27 22:47:17 -0300

    profile 2

--- client/src/components/profile.js
+++ client/src/components/profile.js
@@ -17,18 +17,22 @@ const Profile = () => {
             <Content style={styles.userBoxContainer}>
                 <Space direction="horizontal" size="large">
                     <Content style={styles.avatarContainer}>
-                        <Avatar size="large" gap={5} src={process.env.PUBLIC_URL + 'images/Immortal1'}>
-                        </Avatar>
+                        <Avatar size="large" gap={5} src={process.env.PUBLIC_URL + 'images/Immortal1'}> </Avatar>       
                     </Content>
                     <Content style={styles.nameContainer}>
                         <Typography style={styles.username}>Parradura</Typography>
                     </Content>
                 </Space>
                 <Divider/>
-                <Content>
-                    <Content>
-                        <Image src={process.env.PUBLIC_URL + 'images/Immortal1'}/>
-                    </Content>
+                <Content style={styles.rankContainer}>
+                    <Space direction="horizontal" size="large" >
+                        <Content style={styles.rankImageContainer}>
+                            <Image src={process.env.PUBLIC_URL + 'images/Immortal1.png'} width={125} height={125}/>
+                        </Content>
+                        <Content style={styles.rankNameContainer}>
+                            <Typography style={styles.rankName}>Immortal 1</Typography>
+                        </Content>
+                    </Space>
                 </Content>
             </Content>
         )
@@ -56,6 +60,26 @@ const styles = {
         color:'#fff',
         fontSize:20,
         fontWeight:'bold'
+    },
+    rankContainer:{
+        width:'100%',
+    },
+    rankImageContainer:{
+        //width:'50%',
+        flex:1,
+        backgroundColor:'#ff88'
+
+    },
+    rankNameContainer:{
+        flex:1,
+        alignItems:'center',
+        justifyContent:'center'
+        //width:'50%'
+    },
+    rankName:{
+        color:'#c21717',
+        fontSize:16,
+        fontWeight:'500'
     }
 }
 
