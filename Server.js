85a78a9c8fc6e1bf82c3b73fab0230c839173807
manavi101 <you@example.com>
2021-01-05 17:31:49 -0300

    add mongo model for testing

--- Server.js
+++ Server.js
@@ -1,14 +1,13 @@
 require('dotenv').config()
 const express = require('express');
 const mongoose = require('mongoose');
+require('./models/Weapon');
 const path = require('path');
 const bodyParser = require('body-parser');
 const mongodb = require('MongoDB')
 const app = express();
 //mongoose.Promise = global.Promise;
 mongoose.connect(process.env.DB_MONGODB,{ useNewUrlParser: true });
-let db = mongoose.connect
-app.use(bodyParser.json());
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
@@ -24,24 +23,12 @@ app.get(`/api/hello`, async (req, res) => {
 });
 //app.use(express.static(path.join(__dirname, 'Weapons.json')));
 app.get(`/api/Weapons`, async (req, res) => {
+  
   //res.sendFile(path.join(__dirname,'Weapons.json'));
-  try{
-    let fs = require('fs'); /* Put it where other modules included */
-    let Weapons = JSON.parse( await fs.readFileSync('Weapons.json', 'utf8'));
-    let WeaponsDamages = JSON.parse( await fs.readFileSync('WeaponsDamage.json', 'utf8'));
-    for(let i=0;Weapons.length>i; i++){
-      let aux=0;
-      Weapons[i].WeaponsDamage = [];
-      for(let j=0;WeaponsDamages.length>j; j++){
-          if(WeaponsDamages[j].weaponid===Weapons[i].id){
-              Weapons[i].WeaponsDamage[aux] = Object.assign(WeaponsDamages[j])
-              aux++;
-          }
-      }
-    }
+    const Weapon = mongoose.model('Weapon');
+    let fs = require('fs');
+    let Weapons = JSON.parse(fs.readFileSync('Weapons.json', 'utf8'));
     res.send(Weapons);
-  } catch(err) {
-    res.status(500).send()
-  }
+    Weapon.create(Weapons);
   
 });
---
+++ models/Weapon.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const {Schema} = mongoose;
+
+const WeaponSchema = new Schema({
+    _id:            {type: String, required:true},
+    name:           {type: String, required:true},
+    category:       {type: String, required:false},
+    firemode:       {type: String, required:false},
+    firerate:       {type: String, required:false},
+    altfire:        {type: Boolean, required:false},
+    altfiremode:    {type: String, required:false},
+    altfireratio:   {type: String, required:false},
+    wallpen:        {type: String, required:false},
+    magazine:       {type: Number, required:false},
+    price:          {type: Number, required:false},
+    img:            {type: String, required:false},
+    hidden:         {type: Boolean, default:false},
+    damage:         [{order:Number,distance:String,head:Number,body:Number,legs:Number}]
+})
+
+mongoose.model('Weapon', WeaponSchema);
