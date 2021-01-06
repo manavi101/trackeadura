bdac54dfc2bee796c3efe5d6e1f9f17d207ac62a
ignacio.parra <ignacio.parra@minder.cc>
2021-01-06 16:15:08 -0300

    fix getWeapon by id

--- controllers/weapons.js
+++ controllers/weapons.js
@@ -7,17 +7,12 @@ const HttpError = require('../models/http-error');
 const getWeapons = async (req, res, next) => {
   const { id } = req.params;
   if(id){
-    if(!mongoose.Types.ObjectId.isValid(id)) {
-      return next(
-        new HttpError('No se ha encontrado ningún arma con el id especificado.',404)
-      );
-    }
     try {
       let weapon = await Weapon.findById(id);
       res.status(200).json(weapon)
     } catch (error) {
       return next(
-        new HttpError('Ha ocurrido un error al obtener el arma.',400)
+        new HttpError('No se ha encontrado ningún arma con el id especificado.',404)
       );
     }
   } else {
