c403b435a14e37b94747a4c0fd91e56cb7c72cf0
manavi101 <you@example.com>
2021-01-11 13:12:12 -0300

    fixing tags

--- controllers/users.js
+++ controllers/users.js
@@ -1,15 +1,15 @@
 const mongoose = require('mongoose');
-let Weapon = require('../models/weapon.model');
+let User = require('../models/users.model');
 const HttpError = require('../models/http-error');
 
 const getUser = async (req, res, next) => {
     const { name, tag } = req.params;
     if(name&&tag){
       try {
-        let weapon;
-        weapon = await Weapon.findOne({name:name},{tag:tag})
-        if(weapon){
-          res.status(200).json(weapon);
+        let user;
+        user = await User.findOne({name:name},{tag:tag})
+        if(user){
+          res.status(200).json(user);
         }else{
           return next(
             new HttpError('There isn´t a user with name = '+name+'#'+tag,404)
