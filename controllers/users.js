4b7aee463a90f4b3b8aab7799a8261a8ccceda71
manavi101 <you@example.com>
2021-01-11 21:37:32 -0300

    Fixing errors on getExternaluser

--- controllers/users.js
+++ controllers/users.js
@@ -17,20 +17,22 @@ const getUser = async (req, res, next) => {
           await axios.get('https://valorant.iesdev.com/player/'+name+'-'+tag)
           .then(function(response){
             //if exists is saved in db for future searches
-            console.log(response.data)
             user = response.data
             user = User({_id: user.id,name: user.name,tag: user.tag, puuid: user.puuid})
             user.save();
             res.status(200).json(user);
           })
           .catch(function(error){
-            return next(
-              new HttpError('There isn´t a user with name = '+name+'#'+tag+'\n'+ error,404)
-            );
+            if(error.response.status===404){
+              return next(
+                new HttpError('There isn´t a user with name = '+name+'#'+tag+'\n',404)
+              );
+            }else{
+              return next(
+                new HttpError('Something went wrong: '+error,500)
+              );
+            }
           })
-          /*return next(
-            new HttpError('There isn´t a user with name = '+name+'#'+tag,404)
-          );*/
       }
     } catch (error) {
       return next(
